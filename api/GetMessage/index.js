e20a77b0bce975dc768f6b38dd76cfea13ac9495
Annaji Sharma Ganti <42851022+annaji-msft@users.noreply.github.com>
2021-02-12 10:46:09 -0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,10 @@
 module.exports = async function (context, req) {
+  var siteName = process.env.WEBSITE_SITE_NAME;
+  var packageUrl = process.env.WEBSITE_RUN_FROM_PACKAGE;
   context.res = {
-    // status: 200, /* Defaultss to 200 */
-    body: { text: "Hello from the API" },
+    // status: 200, /* Defaults to 200 */
+    body: {
+      text: `siteName: ${siteName} packageUrl: ${packageUrl}`
+    },
   };
 };
